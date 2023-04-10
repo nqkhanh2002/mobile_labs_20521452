8b0be018ee165803fc4c3006de9f561adb165d2b
Nguyen Quoc Khanh <20521452@gm.uit.edu.vn>
2023-04-11 04:01:29 +0700

    ininital

--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.mobileproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView Lab02;
+    GridView Lab02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
